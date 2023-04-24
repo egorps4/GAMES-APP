b709278bb8bf26de9ad7bea3b96814ea8a6d0bb5
egorps4~ <egorps4@gmail.com>
2023-04-24 12:13:17 +0400

    Исправления

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import * as flsFunction from "./modules/functions.js";
 flsFunction.isWebp();
 
 const API_KEY = "e0fdba8f84e74c8fa0a3416de562b21f",
-      API_URL = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=21`; 
+      API_URL = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=21&search_exact&ordering=-metacritic`; 
 
 async function getGames(url) {
     const response = await fetch(url, {
@@ -15,8 +15,6 @@ async function getGames(url) {
     const responseData = await response.json();
     showGames(responseData);
 }
-
-getGames(API_URL);
 
 function getColor(ratingColor) {
     if (ratingColor >= 70) {
@@ -36,20 +34,21 @@ function getRating(rating) {
 
 function getImage(image) {
     if (!image) {
-        image = '../img/01.jpg';
+        image = 'img/01.jpg';
     }
     return image;
 }
 
-
 function showGames(data) {
     const gamesContainer = document.querySelector('.cards__container');
 
-    document.querySelector('.cards__container').innerHTML = '';
+    gamesContainer.innerHTML = '';
 
     data.results.forEach(game => {
         const gameCard = document.createElement('div');
+
         gameCard.classList.add('cards__item');
+
         gameCard.innerHTML = `
             <div class="cards__image"> 
                 <img class="cards__img" src="${getImage(game.background_image)}" alt="${game.name}">
@@ -60,9 +59,16 @@ function showGames(data) {
                 <div class="cards__genre">${game.genres.map(genre => ` ${genre.name}`)}</div>
             </div>
         `
+
         gamesContainer.appendChild(gameCard);
     });
+
+    if (gamesContainer.children.length === 0) {
+        gamesContainer.innerHTML = 'Ничего не найдено'
+    }
 };
+
+getGames(API_URL);
 
 const searchForm = document.querySelector('.header__form'),
       search = document.querySelector('.header__search');
@@ -71,8 +77,8 @@ searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const apiSearch = `${API_URL}&search=${search.value}`; 
+    
     if (search.value) {
-        console.log(apiSearch);
         getGames(apiSearch);
     }
 });
